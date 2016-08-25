e02d5a95c1f325c09d92b7c678f6a38a9e1720ec
Alexey Plutalov <demiazz.py@gmail.com>
2016-08-25 16:42:03 +0300

    Add aliasing of elements with `data-role` attributes

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -13,7 +13,7 @@ describe('lighty-plugin-legacy', () => {
 
   afterEach(clear);
 
-  it('bind `$(node)` to `this.block`', (done) => {
+  it('bind `$(node)` to `this.block`', done => {
     fixture('<div class="bind"></div>');
 
     application.component('.bind', {
@@ -28,7 +28,7 @@ describe('lighty-plugin-legacy', () => {
     application.vitalize();
   });
 
-  it('bind `$(node).find` to `this.$`', (done) => {
+  it('bind `$(node).find` to `this.$`', done => {
     fixture(`
       <div class="outside element">
       <div class="find">
@@ -49,5 +49,75 @@ describe('lighty-plugin-legacy', () => {
     });
 
     application.vitalize();
+  });
+
+  describe('role aliases', () => {
+    it('creates properties for each element with `data-role`', done => {
+      fixture(`
+        <div data-role="single"></div>
+        <div data-role="multi"></div>
+        <div data-role="customRole"></div>
+        <div data-role="yet-another-role"></div>
+        <div class="role">
+          <div data-role="single"></div>
+          <div data-role="multi other"></div>
+          <div data-role="multi"></div>
+          <div data-role="customRole"></div>
+          <div data-role="yet-another-role"></div>
+          <div>
+            <div data-role="single"></div>
+            <div data-role="multi other"></div>
+            <div data-role="multi"></div>
+            <div data-role="customRole"></div>
+            <div data-role="yet-another-role"></div>
+          </div>
+        </div>
+      `);
+
+      application.component('.role', {
+        init() {
+          expect(this.single.length).toEqual(2);
+          expect(this.single.toArray())
+            .toEqual(this.$('@single').toArray());
+
+          expect(this.multi.length).toEqual(4);
+          expect(this.multi.toArray())
+            .toEqual(this.$('@multi').toArray());
+
+          expect(this.customRole.length).toEqual(2);
+          expect(this.customRole.toArray())
+            .toEqual(this.$('@customRole').toArray());
+
+          expect(this['yet-another-role'].length).toEqual(2);
+          expect(this['yet-another-role'].toArray())
+            .toEqual(this.$('@yet-another-role').toArray());
+
+          done();
+        },
+      });
+
+      application.vitalize();
+    });
+
+    // eslint-disable-next-line
+    it("doesn't create aliases for existing properties", done => {
+      fixture(`
+        <div class="role-no-bind">
+          <div data-role="exists" />
+        </div>
+      `);
+
+      application.component('.role-no-bind', {
+        exists: 'string',
+
+        init() {
+          expect(this.exists).toEqual('string');
+
+          done();
+        },
+      });
+
+      application.vitalize();
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 import $ from 'jquery';
 import { plugin } from 'lighty';
 
+import './roles';
+
 
 function pluginInitializer() {
   return function transform(component, node) {
@@ -11,6 +13,25 @@ function pluginInitializer() {
     component.$ = function find(selector) {
       return $(node).find(selector);
     };
+
+    const elementsWithRole = component.$('[data-role]');
+
+    for (let i = 0; i < elementsWithRole.length; i++) {
+      const elementWithRole = elementsWithRole[i];
+      const roles = elementWithRole.attributes['data-role'].value.split(' ');
+
+      for (let j = 0; j < roles.length; j++) {
+        const role = roles[j];
+
+        if (!component[role]) {
+          component[role] = $();
+        }
+
+        if (component[role].jquery) {
+          component[role].push(elementWithRole);
+        }
+      }
+    }
   };
 }
 
