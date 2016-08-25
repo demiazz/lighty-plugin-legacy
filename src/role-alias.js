9e16cf1893ec0435a4eae8c4277b3885e20c4a05
Alexey Plutalov <demiazz.py@gmail.com>
2016-08-25 18:05:22 +0300

    Fix roles aliasing

--- src/role-alias.js
+++ src/role-alias.js
@@ -4,14 +4,14 @@ import $ from 'jquery';
 
 
 export default function addRoleAlias(component) {
-  const nodes = component.querySelector('[data-role]');
+  const nodes = component.$('[data-role]');
 
   for (let i = 0; i < nodes.length; i++) {
-    const node = nodes.item(i);
+    const node = nodes[i];
     const roles = node.attributes['data-role'].value.split(' ');
 
     for (let j = 0; j < roles.length; j++) {
-      const role = roles[i];
+      const role = roles[j];
 
       if (!component[role]) {
         component[role] = $();
